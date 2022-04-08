7dd27d2fcd4822b75af6bf18deca11d2ba89daa9
Abraham851 <smishu851@gmail.com>
2022-04-08 17:44:55 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from 'react';
+
 
 
 
@@ -9,23 +11,64 @@ const auth = getAuth(app);
 
 
 function App() {
-  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
 
   const handleGoogleSingIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then(result => {
         const user = result.user;
-        console.log(user);
+        setUser(user);
+        // console.log(user);
       })
       .catch(error => {
         console.log(error);
       })
   }
+  const handleGithubSingIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        // console.log('user')
+
+      })
+      .catch('error', error => {
+
+      })
+
+  }
+
+  const handleSingOut = () => {
+    signOut(auth,)
+      .then(() => {
+        setUser({});
+
+      })
+      .catch(error => {
+        setUser({});
+      })
+
+  }
 
   return (
     <div className="App">
 
-      <button onClick={handleGoogleSingIn}>Google Sing In</button>
+      {
+        user.uid ? <button className='sing-out' onClick={handleSingOut}>Sing Out</button> :
+          <>
+            <button className='sing-in' onClick={handleGoogleSingIn}>Google Sing In</button>
+            <button className='sing-in' onClick={handleGithubSingIn}>GitHub sing In</button>
+
+          </>
+
+      }
+
+      <h2>YOUR NAME: {user.displayName}</h2>
+      <p>YOUE EMAIL: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 
